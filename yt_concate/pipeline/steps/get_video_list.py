21a6f6555a2fba5d38e5f792b0b6ad4a437e5d5b
suhunlin <lin.suhun@msa.hinet.net>
2022-06-22 16:24:48 +0800

    refactor code into pipeline design pattern

---
+++ yt_concate/pipeline/steps/get_video_list.py
@@ -0,0 +1,43 @@
+import urllib.request
+import json
+import os
+
+from yt_concate.pipeline.steps.Step import Step
+
+
+class GetVideoList(Step):
+
+    def __init__(self):
+        print('GetVideoList was born!!!')
+
+    def process(self, data, inputs):
+
+        api_key = os.getenv('API_KEY')
+        channel_id = inputs['channel_id']
+
+        base_video_url = 'https://www.youtube.com/watch?v='
+        base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
+
+        first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(api_key,
+                                                                                                            channel_id)
+
+        video_links = []
+        url = first_url
+        while True:
+            inp = urllib.request.urlopen(url)
+            resp = json.load(inp)
+            for i in resp['items']:
+                if i['id']['kind'] == "youtube#video":
+                    video_links.append(base_video_url + i['id']['videoId'])
+
+            try:
+                next_page_token = resp['nextPageToken']
+                url = first_url + '&pageToken={}'.format(next_page_token)
+            except KeyError:
+                break
+        print(video_links)
+        return video_links
+
+
+class StepException(Exception):
+    pass
